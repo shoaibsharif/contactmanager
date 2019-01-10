56bd2cb49716c559f13a2e83b773dbd779424577
Shoaib <shoaib19j@gmail.com>
2019-01-10 15:14:20 -0800

    Updates

---
+++ precache-manifest.ea2585be9a5d523e65643530c0476031.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ef19ad62caf91a81e526",
+    "url": "/static/js/main.ef19ad62.chunk.js"
+  },
+  {
+    "revision": "2ca539a6459eded07b35",
+    "url": "/static/js/1.2ca539a6.chunk.js"
+  },
+  {
+    "revision": "ef19ad62caf91a81e526",
+    "url": "/static/css/main.4fc41a1f.chunk.css"
+  },
+  {
+    "revision": "2ca539a6459eded07b35",
+    "url": "/static/css/1.14225832.chunk.css"
+  },
+  {
+    "revision": "a4cb589536beadb9184763a1bc80eccf",
+    "url": "/index.html"
+  }
+];
